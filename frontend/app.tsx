d09ca5d375a76fc0671a69b5414aa2ac24dee81a
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-03 20:54:27 +0100

    added start of CSS

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,15 +1,24 @@
+import {add, render} from "@css";
 import {amendNode} from "@dom";
 import ready from "@load";
 import {router} from "@router";
 import {projects} from "./endpoints.js";
 
+add({
+	"#projects": {
+		"list-style": "none",
+		"padding-left": 0,
+	}
+});
+
 ready.then(projects).then(projects => {
+	amendNode(document.head, render());
 	amendNode(document.body, <>
 		<h1 style={{"font-family": "arial"}}>ΑΣΞΠΩ</h1>
 		{router().add("/project/:id", ({id}) => {
 			return <div>Project: {id}</div>
 		}).add("", () => {
-			return <ul>
+			return <ul id="projects">
 				{projects.map(project => <li><a href={"/project/"+project}>{project}</a></li>)}
 			</ul>
 		})}
