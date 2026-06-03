65f820e52c43b4fbbf49e8276b4734396fd6ac85
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-03 19:43:56 +0100

    correct 'href' typing

--- frontend/jsx.d.ts
+++ frontend/jsx.d.ts
@@ -8,10 +8,11 @@ declare namespace JSX {
 	type AllElementsMap = HTMLElementTagNameMap & SVGElementTagNameMap & MathMLElementTagNameMap;
 
 	type IntrinsicElements = {
-		[K in keyof AllElementsMap]: Omit<Partial<AllElementsMap[K]>, "style"> & {
+		[K in keyof AllElementsMap]: Omit<Partial<AllElementsMap[K]>, "style" | "href"> & {
 			[ev: `on${string}`]: EventListenerObject | Function;
 			[attribute: string]: unknown;
 			"style"?: Record<string, ToString | undefined> | CSSStyleDeclaration | string;
+			"href"?: unknown;
 		}
 	}
 }
