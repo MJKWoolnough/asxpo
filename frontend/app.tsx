0f718b65fb9e2001c493eafa49e6471f64e48140
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-03 20:44:37 +0100

    use projects endpoint to get list of projects

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,14 +1,17 @@
 import {amendNode} from "@dom";
 import ready from "@load";
 import {router} from "@router";
+import {projects} from "./endpoints.js";
 
-ready.then(() => {
+ready.then(projects).then(projects => {
 	amendNode(document.body, <>
 		<h1 style={{"font-family": "arial"}}>ΑΣΞΠΩ</h1>
 		{router().add("/project/:id", ({id}) => {
 			return <div>Project: {id}</div>
 		}).add("", () => {
-			return <a href="/project/abc">ABC</a>
+			return <ul>
+				{projects.map(project => <li><a href={"/project/"+project}>{project}</a></li>)}
+			</ul>
 		})}
 	</>)
 });
