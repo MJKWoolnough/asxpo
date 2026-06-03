386a65cec0c563ca46593048ac267a7fb8a3c247
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-03 20:55:07 +0100

    pull out complicated IntrinsicElements typing to keep type hover text clean

--- frontend/jsx.d.ts
+++ frontend/jsx.d.ts
@@ -2,18 +2,20 @@ interface ToString {
 	toString(): string;                                                                                                                                                           
 }
 
+type ElementType<T extends Element> = Omit<Partial<T>, "style" | "href"> & {
+	[ev: `on${string}`]: EventListenerObject | Function;
+	[attribute: string]: unknown;
+	"style"?: Record<string, ToString | undefined> | CSSStyleDeclaration | string;
+	"href"?: unknown;
+}
+
 declare namespace JSX {
 	type Element = HTMLElement | SVGElement | MathMLElement;
 
 	type AllElementsMap = HTMLElementTagNameMap & SVGElementTagNameMap & MathMLElementTagNameMap;
 
 	type IntrinsicElements = {
-		[K in keyof AllElementsMap]: Omit<Partial<AllElementsMap[K]>, "style" | "href"> & {
-			[ev: `on${string}`]: EventListenerObject | Function;
-			[attribute: string]: unknown;
-			"style"?: Record<string, ToString | undefined> | CSSStyleDeclaration | string;
-			"href"?: unknown;
-		}
+		[K in keyof AllElementsMap]: ElementType<AllElementsMap[K]>;
 	}
 }
 
