2163dcd1fcb21e3437297f12b2b60905f83b631f
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-03 19:44:05 +0100

    start of router code

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,7 +1,14 @@
+import {amendNode} from "@dom";
 import ready from "@load";
+import {router} from "@router";
 
 ready.then(() => {
-	document.body.append(
+	amendNode(document.body, <>
 		<h1 style={{"font-family": "arial"}}>ΑΣΞΠΩ</h1>
-	)
+		{router().add("/project/:id", ({id}) => {
+			return <div>Project: {id}</div>
+		}).add("", () => {
+			return <a href="/project/abc">ABC</a>
+		})}
+	</>)
 });
