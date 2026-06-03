8981a07e2c3bcd4ed7c9fd9da208558a460985c4
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-03 20:43:16 +0100

    added basic endpoint to return exiting project list

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
 	"text/template"
@@ -9,7 +10,16 @@ import (
 )
 
 func main() {
-	jsx := template.Must(template.New("").Parse("{{if .InHTML}}import {TAG_NAME} from '@html';{{else if .InSVG}}import {TAG_NAME} from '@svg';{{end}}TAG_NAME({{if .HasParams}}PARAMS{{end}}{{if .HasChildren}}{{if .HasParams}},{{end}}CHILDREN{{end}})"))
+	http.Handle("GET /", http.FileServer(http.FS(tsserver.WrapFS(os.DirFS("./frontend/"), tsserver.JSX(template.Must(template.New("").Parse("{{if .InHTML}}import {TAG_NAME} from '@html';{{else if .InSVG}}import {TAG_NAME} from '@svg';{{end}}TAG_NAME({{if .HasParams}}PARAMS{{end}}{{if .HasChildren}}{{if .HasParams}},{{end}}CHILDREN{{end}})")))))))
+	http.Handle("GET /api/projects", http.HandlerFunc(projects))
 
-	http.ListenAndServe(":8080", http.FileServer(http.FS(tsserver.WrapFS(os.DirFS("./frontend/"), tsserver.JSX(jsx)))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func projects(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode([]string{
+		"ProjectA",
+		"ProjectB",
+	})
 }
