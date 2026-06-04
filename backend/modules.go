2ade03e24515da095b7f38081be3e1cf90537a78
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-04 07:46:10 +0100

    added ListModules endpoint

--- backend/modules.go
+++ backend/modules.go
@@ -42,4 +42,31 @@ func (b *Backend) createModule(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+func (b *Backend) ListModules(w http.ResponseWriter, r *http.Request) {
+	handle(b.listModules, w, r)
+}
+
+func (b *Backend) listModules(w http.ResponseWriter, r *http.Request) error {
+	paths, err := filepath.Glob(filepath.Join(b.path, "*", "description"))
+	if err != nil {
+		return err
+	}
+
+	modules := make([]newModule, 0, len(paths))
+
+	for _, path := range paths {
+		name := filepath.Base(filepath.Dir(path))
+		description, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		modules = append(modules, newModule{Name: name, Description: string(description)})
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	return json.NewEncoder(w).Encode(modules)
+}
+
 var ErrInvalidName = errors.New("module names cannot contain slashes or null bytes")
