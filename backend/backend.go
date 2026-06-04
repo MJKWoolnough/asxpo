90def2e1a36f0019dd9cd4b99340ae939784deb5
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-04 07:10:39 +0100

    add HTTP buffer handler

--- backend/backend.go
+++ backend/backend.go
@@ -1,5 +1,11 @@
 package backend
 
+import (
+	"net/http"
+
+	"vimagination.zapto.org/httpbuffer"
+)
+
 type Backend struct {
 	path string
 }
@@ -8,4 +14,24 @@ func New(path string) *Backend {
 	return &Backend{
 		path: path,
 	}
+}
+
+func handle(fn func(http.ResponseWriter, *http.Request) error, w http.ResponseWriter, r *http.Request) {
+	httpbuffer.Handler{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if err := fn(w, r); err != nil {
+				http.Error(w, err.Error(), responseCode(err))
+			}
+		}),
+	}.ServeHTTP(w, r)
+}
+
+var httpErrors = map[error]int{}
+
+func responseCode(err error) int {
+	if c, ok := httpErrors[err]; ok {
+		return c
+	}
+
+	return http.StatusInternalServerError
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module vimagination.zapto.org/asxpo
 
 go 1.26.3
 
-require vimagination.zapto.org/tsserver v1.3.1
+require (
+	vimagination.zapto.org/httpbuffer v1.1.4
+	vimagination.zapto.org/tsserver v1.3.1
+)
 
 require (
+	vimagination.zapto.org/httpencoding v1.1.1 // indirect
+	vimagination.zapto.org/httpwrap v1.0.1 // indirect
 	vimagination.zapto.org/javascript v1.7.1 // indirect
 	vimagination.zapto.org/parser v1.2.3 // indirect
 )
