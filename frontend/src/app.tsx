917aeb9ef75ded9f4b01d48a84ae2f7b608d8b5b
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-05 08:23:00 +0100

    added simple Create Module UI

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,8 +1,8 @@
 import {add, render} from "@css";
 import {amendNode} from "@dom";
 import ready from "@load";
-import {router} from "@router";
-import {modules} from "./endpoints.js";
+import {goto, router} from "@router";
+import {createModule, modules} from "./endpoints.js";
 
 add({
 	"#modules": {
@@ -18,9 +18,25 @@ ready.then(modules).then(modules => {
 		{router().add("/modules/:id", ({id}) => {
 			return <div>Module: {id}</div>
 		}).add("", () => {
-			return <ul id="modules">
-				{modules.map(m => <li><a href={"/modules/"+m.Name}>{m.Name}</a></li>)}
-			</ul>
+			return <div>
+				<button onclick={() => {
+					const name = prompt("Enter module name:"),
+					      description = prompt("Enter module description:");
+
+					if (!name) {
+						alert("Name required");
+
+						return;
+					}
+
+					createModule(name, description ?? "")
+					.then(() => goto("/modules/"+name))
+					.catch(e => alert("Failed to create environment: " + e.message))
+				}}>Create Module</button>
+				<ul id="modules">
+					{modules.map(m => <li><a href={"/modules/"+m.Name}>{m.Name}</a></li>)}
+				</ul>
+			</div>
 		})}
 	</>)
 });
