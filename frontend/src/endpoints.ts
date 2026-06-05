7dde753c6f4d1b8305fcbdc1080681649e2b10eb
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-05 08:14:55 +0100

    added endpoint to create modules

--- frontend/src/endpoints.ts
+++ frontend/src/endpoints.ts
@@ -1,4 +1,5 @@
 import {HTTPRequest} from "@conn";
 import {Arr, Obj, Str} from "@typeguard";
 
-export const modules = () => HTTPRequest("/api/modules", {"response": "json", "checker": Arr(Obj({"Name": Str(), "Description": Str()}))});
+export const modules = () => HTTPRequest("/api/modules", {"response": "json", "checker": Arr(Obj({"Name": Str(), "Description": Str()}))}),
+	createModule = (name: string, description: string) => HTTPRequest("/api/modules/"+name, {"method": "put", "data": description});
