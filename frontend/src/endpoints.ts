5d03a9a9ad1f3f0130ec81863c5ead386f052f50
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-05 08:44:54 +0100

    added DELETE endpoint code

--- frontend/src/endpoints.ts
+++ frontend/src/endpoints.ts
@@ -2,4 +2,5 @@ import {HTTPRequest} from "@conn";
 import {Arr, Obj, Str} from "@typeguard";
 
 export const modules = () => HTTPRequest("/api/modules", {"response": "json", "checker": Arr(Obj({"Name": Str(), "Description": Str()}))}),
-	createModule = (name: string, description: string) => HTTPRequest("/api/modules/"+name, {"method": "put", "data": description});
+createModule = (name: string, description: string) => HTTPRequest("/api/modules/"+name, {"method": "put", "data": description}),
+deleteModule = (name: string) => HTTPRequest("/api/modules/"+name, {"method": "put"});
