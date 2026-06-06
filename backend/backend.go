af5185b29e165de7252aad71b0cfb1aa61b98981
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-06 18:16:27 +0100

    ErrInvalidName should be code 422

--- backend/backend.go
+++ backend/backend.go
@@ -41,6 +41,7 @@ func handle(fn func(http.ResponseWriter, *http.Request) error, w http.ResponseWr
 
 var httpErrors = map[error]int{
 	ErrDuplicateName: http.StatusConflict,
+	ErrInvalidName:   http.StatusUnprocessableEntity,
 	ErrNoModule:      http.StatusNotFound,
 }
 
