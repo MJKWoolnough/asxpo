a208eada2ebdeaf0f77a88c83c9f8d02b7964933
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-08 07:45:42 +0100

    simplified JSX template

--- frontend/index_dev.go
+++ frontend/index_dev.go
@@ -10,4 +10,4 @@ import (
 	"vimagination.zapto.org/tsserver"
 )
 
-var Index = http.FileServer(http.FS(tsserver.WrapFS(os.DirFS("./frontend/src/"), tsserver.JSX(template.Must(template.New("").Parse(`{{if and .InHTML (eq .Namespace "html") }}import {TAG_NAME} from "@html";{{else if and .InSVG (eq .Namespace "svg")}}import {TAG_NAME} from "@svg";{{end}}TAG_NAME({{if .HasParams}}PARAMS{{end}}{{if .HasChildren}}{{if .HasParams}},{{end}}CHILDREN{{end}})`))))))
+var Index = http.FileServer(http.FS(tsserver.WrapFS(os.DirFS("./frontend/src/"), tsserver.JSX(template.Must(template.New("").Parse(`{{if or .InHTML .InSVG }}import {TAG_NAME} from "@{{.Namespace}}";{{end}}TAG_NAME({{if .HasParams}}PARAMS{{end}}{{if .HasChildren}}{{if .HasParams}},{{end}}CHILDREN{{end}})`))))))
