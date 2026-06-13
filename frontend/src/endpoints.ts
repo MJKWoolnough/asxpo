39015d142c7e1d6a2fed647bf1a10b643497cee7
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-13 20:22:45 +0100

    added temp setType endpoint (+ required Type)

--- frontend/src/endpoints.ts
+++ frontend/src/endpoints.ts
@@ -1,8 +1,18 @@
+import type {TypeGuardOf} from "@typeguard";
 import {HTTPRequest} from "@conn";
 import {Arr, Obj, Str} from "@typeguard";
+
+const Field = Obj({
+	"Name": Str(),
+	"Type": Str(),
+	"Description": Str(),
+});
+
+export type Field = TypeGuardOf<typeof Field>;
 
 export const modules = () => HTTPRequest("/api/modules", {"response": "json", "checker": Arr(Obj({"Name": Str(), "Description": Str()}))}),
 setModule = (name: string, description: string) => HTTPRequest("/api/modules/"+name, {"method": "put", "data": description}),
 getModule = (name: string) => HTTPRequest("/api/modules/"+name),
 renameModule = (name: string, newName: string) => HTTPRequest(`/api/modules/${name}/${newName}`, {"method": "post"}),
-deleteModule = (name: string) => HTTPRequest("/api/modules/"+name, {"method": "delete"});
+deleteModule = (name: string) => HTTPRequest("/api/modules/"+name, {"method": "delete"}),
+setType = (module: string, name: string, description: string, typ: Field[]) => HTTPRequest(`/api/modules/${module}/${name}`, {"method": "put", "data": JSON.stringify({description, "type": typ})});
