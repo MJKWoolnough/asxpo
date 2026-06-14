2e50c521b2a27cd316cb8097e1a255e4e11aa914
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-14 17:54:02 +0100

    use new Form element for creating types

--- frontend/src/module.tsx
+++ frontend/src/module.tsx
@@ -4,6 +4,7 @@ import CSS from "@css";
 import {bindCustomElement} from "@dom";
 import {goto} from "@router";
 import {getModule, setModule, setType} from "./endpoints.js";
+import Form from "./form.js";
 import Shadow from "./shadow.js";
 import {Add, Edit, Remove} from "./symbols.js";
 
@@ -29,34 +30,35 @@ const id = Object.freeze(["id"]),
 		}
 	}
 })],
-      Fields = ({fields, types}: {"fields": Field[], "types": string[]}) => {
+      Fields = ({types}: {"types": string[]}) => {
 	const adder = <li>
 		<button type="button" onclick={() => {
-		const field = {"Name": "", "Type": "", "Description": ""};
+			const field = {"Name": "", "Type": "", "Description": ""};
 
-		fields.push(field);
+			fields.push(field);
 
-		adder.before(<li>
-			<input type="text" onchange={function(this: HTMLInputElement) {field.Name = this.value}} placeholder="Name" required />
-			<input type="text" onchange={function(this: HTMLInputElement) {field.Type = this.value}} placeholder="Type" required list="types" />
-			<input type="text" onchange={function(this: HTMLInputElement) {field.Type = this.value}} placeholder="Description" />
-			<button type="button" onclick={function(this: HTMLButtonElement) {
-				const index = fields.indexOf(field);
+			adder.before(<li>
+				<input type="text" onchange={function(this: HTMLInputElement) {field.Name = this.value}} placeholder="Name" required />
+				<input type="text" onchange={function(this: HTMLInputElement) {field.Type = this.value}} placeholder="Type" required list="types" />
+				<input type="text" onchange={function(this: HTMLInputElement) {field.Type = this.value}} placeholder="Description" />
+				<button type="button" onclick={function(this: HTMLButtonElement) {
+					const index = fields.indexOf(field);
 
-				if (index >= 0) {
-					fields.splice(index, 1);
-				}
+					if (index >= 0) {
+						fields.splice(index, 1);
+					}
 
-				this.parentElement?.remove();
-			}}><Remove title="Remove Field" /></button>
-		</li>);
+					this.parentElement?.remove();
+				}}><Remove title="Remove Field" /></button>
+			</li>);
 		}}>+</button>
 		<datalist id="types">
 			{types.map(t => <option>{t}</option>)}
 		</datalist>
-	</li> as HTMLLIElement;
+	</li> as HTMLLIElement,
+	      fields: Field[] = [];
 
-	return <ul>{adder}</ul>;
+	return Object.assign(<ul label="Fields">{adder}</ul>, {"value": fields});
       },
       types: string[] = [
 	      "string",
@@ -74,72 +76,18 @@ export default bindCustomElement("aspxo-module", class Module extends HTMLElemen
 			<div>
 				<h2>{this.#name}</h2>
 				<pre>{this.#description}</pre>
-				<button onclick={() => {
-					const desc = <textarea id="module_description" value={this.#description()} />,
-					      fs = <fieldset>
-						<legend>Edit Description</legend>
-						<label for="module_description">Description:</label>{desc}
-						<div>
-							<button type="submit">Update</button>
-							<button type="button" commandfor="module_desc" command="close">Cancel</button>
-						</div>
-					      </fieldset> as HTMLFieldSetElement,
-					      overlay = <dialog id="module_desc" onclose={() => overlay.remove()} closedby="any">
-						<form onsubmit={(e: Event) => {
-							e.preventDefault();
-	
-							fs.disabled = true;
-	
-							setModule(this.#name(), desc.value)
-							.then(() => {
-								this.#description(desc.value);
-								overlay.close();
-							})
-							.catch(e => {
-								alert("Failed to update description: " + e.message);
-
-								fs.disabled = false;
-							});
-						}}>{fs}</form>
-					      </dialog>;
-	
-					document.body.append(overlay);
-					overlay.showModal();
-				}}><Edit title="Edit Description"/></button>
+				<button onclick={() => <Form legend="Edit Description" submit="Update" onsubmit={(desc: string) => setModule(this.#name(), desc)} onsuccess={this.#description}>
+					<textarea label="Description" value={this.#description()} />,
+				</Form>}><Edit title="Edit Description"/></button>
 			</div>
 			<button onclick={() => {
-				const name = <input id="type_name" required pattern="^[^\/]+$" title="Cannot contain slashes" placeholder=" " />,
-				      desc = <textarea id="type_description" />,
-				      fields: Field[] = [],
-				      fs = <fieldset>
-					<legend>Add Type</legend>
-					<label for="type_name">Name:</label>{name}
-					<label for="type_description">Description:</label>{desc}
-					<label>Fields:</label>
-					<Fields fields={fields} types={types} />
-					<div>
-						<button type="submit">Update</button>
-						<button type="button" commandfor="type_add" command="close">Cancel</button>
-					</div>
-				      </fieldset> as HTMLFieldSetElement,
-				      overlay = <dialog id="type_add" onclose={() => overlay.remove()} closedby="any">
-					<form onsubmit={(e: Event) => {
-						e.preventDefault();
+				const name = <input label="Name" required pattern="^[^\/]+$" title="Cannot contain slashes" placeholder=" " />;
 
-						fs.disabled = true;
-
-						setType(this.#name(), name.value, desc.value, fields)
-						.then(() => overlay.close())
-						.catch(e => {
-							alert("Failed to create type: " + e.message);
-
-							fs.disabled = false;
-						});
-					}}>{fs}</form>
-				      </dialog>;
-
-				document.body.append(overlay);
-				overlay.showModal();
+				<Form legend="Add Type" submit="Add" onsubmit={(name: string, desc: string, fields: Field[]) => setType(this.#name(), name, desc, fields)}>
+					{name}
+					<textarea label="Description" />
+					<Fields types={types} />
+				</Form>
 			}}><Add title="Add Type" /></button>
 		</Shadow>
 	}
