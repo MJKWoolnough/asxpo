7cb7923ac0af55a715596336477b904019674fc4
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-14 17:58:16 +0100

    print transpilation errors in Dev mode

--- frontend/index_dev.go
+++ frontend/index_dev.go
@@ -3,6 +3,8 @@
 package frontend
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"text/template"
@@ -10,4 +12,6 @@ import (
 	"vimagination.zapto.org/tsserver"
 )
 
-var Index = http.FileServer(http.FS(tsserver.WrapFS(os.DirFS("./frontend/src/"), tsserver.JSX(template.Must(template.New("").Parse(`{{if or .InHTML .InSVG }}import {TAG_NAME} from "@{{.Namespace}}";{{end}}TAG_NAME({{if .HasParams}}PARAMS{{end}}{{if .HasChildren}}{{if .HasParams}},{{end}}CHILDREN{{end}})`))))))
+var Index = http.FileServer(http.FS(tsserver.WrapFS(os.DirFS("./frontend/src/"), tsserver.JSX(template.Must(template.New("").Parse(`{{if or .InHTML .InSVG }}import {TAG_NAME} from "@{{.Namespace}}";{{end}}TAG_NAME({{if .HasParams}}PARAMS{{end}}{{if .HasChildren}}{{if .HasParams}},{{end}}CHILDREN{{end}})`))), tsserver.ErrFn(func(w io.Writer, err error) {
+	fmt.Fprintln(w, err)
+}))))
