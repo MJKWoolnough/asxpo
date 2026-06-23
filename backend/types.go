8326322b5c6a9313e8058aaf68035b9832244ab9
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-23 07:30:57 +0100

    implemented setType

--- backend/types.go
+++ backend/types.go
@@ -3,6 +3,9 @@ package backend
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type field struct {
@@ -18,9 +21,36 @@ type mType struct {
 }
 
 func (b *backend) setType(w http.ResponseWriter, r *http.Request) error {
+	moduleName := r.PathValue("module")
+
+	if strings.ContainsAny(moduleName, "/\x00") {
+		return ErrInvalidName
+	}
+
 	var t mType
 
 	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		return err
+	}
+
+	t.Name = r.PathValue("name")
+
+	if strings.ContainsAny(t.Name, "/\x00") {
+		return ErrInvalidName
+	}
+
+	f, err := os.Create(filepath.Join(b.path, moduleName, t.Name+".typ"))
+	if err != nil {
+		return err
+	}
+
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		f.Close()
+
+		return err
+	}
+
+	if err := f.Close(); err != nil {
 		return err
 	}
 
