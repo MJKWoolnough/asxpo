9f9e0486b65a6dbcd1f3918496d2946002a278bd
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-24 07:36:05 +0100

    de-dup'd type checkers

--- frontend/src/endpoints.ts
+++ frontend/src/endpoints.ts
@@ -1,21 +1,23 @@
 import type {TypeGuardOf} from "@typeguard";
 import {HTTPRequest} from "@conn";
-import {Arr, Obj, Str} from "@typeguard";
+import {And, Arr, Obj, Str} from "@typeguard";
 
-const Field = Obj({
-	"Name": Str(),
-	"Type": Str(),
-	"Description": Str(),
-});
+const str = Str(),
+      field = Obj({
+	"Name": str,
+	"Type": str,
+	"Description": str,
+      }),
+      nameDescription = Obj({"Name": str, "Description": str});
 
-export type Field = TypeGuardOf<typeof Field>;
+export type Field = TypeGuardOf<typeof field>;
 
-export const modules = () => HTTPRequest("/api/modules", {"response": "json", "checker": Arr(Obj({"Name": Str(), "Description": Str()}))}),
+export const modules = () => HTTPRequest("/api/modules", {"response": "json", "checker": Arr(nameDescription)}),
 setModule = (name: string, description: string) => HTTPRequest("/api/modules/"+name, {"method": "put", "data": description}),
-getModule = (name: string) => HTTPRequest("/api/modules/"+name, {"response": "json", "checker": Obj({"Name": Str(), "Description": Str(), "Types": Arr(Obj({"Name": Str(), "Description": Str()}))})}),
+getModule = (name: string) => HTTPRequest("/api/modules/"+name, {"response": "json", "checker": And(nameDescription, Obj({"Types": Arr(nameDescription)}))}),
 renameModule = (name: string, newName: string) => HTTPRequest(`/api/modules/${name}/${newName}`, {"method": "post"}),
 deleteModule = (name: string) => HTTPRequest("/api/modules/"+name, {"method": "delete"}),
 setType = (module: string, name: string, description: string, typ: Field[]) => HTTPRequest(`/api/modules/${module}/${name}`, {"method": "put", "data": JSON.stringify({description, "type": typ})}),
-getType = (module: string, name: string) => HTTPRequest(`/api/modules/${module}/${name}`, {"response": "json", "checker": Obj({"Name": Str(), "Description": Str(), "Fields": Arr(Field)})}),
+getType = (module: string, name: string) => HTTPRequest(`/api/modules/${module}/${name}`, {"response": "json", "checker": And(nameDescription, Obj({"Fields": Arr(field)}))}),
 renameType = (module: string, name: string, newName: string) => HTTPRequest(`/api/modules/${module}/${name}/${newName}`, {"method": "post"}),
 deleteType = (module: string, name: string) => HTTPRequest(`/api/modules/${module}/${name}`, {"method": "delete"});
