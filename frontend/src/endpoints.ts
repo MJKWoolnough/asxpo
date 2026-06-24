095fb005627e68ac0ffa69b4317770514e9880e0
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-24 07:33:16 +0100

    added remaining type endpoints

--- frontend/src/endpoints.ts
+++ frontend/src/endpoints.ts
@@ -15,4 +15,7 @@ setModule = (name: string, description: string) => HTTPRequest("/api/modules/"+n
 getModule = (name: string) => HTTPRequest("/api/modules/"+name, {"response": "json", "checker": Obj({"Name": Str(), "Description": Str(), "Types": Arr(Obj({"Name": Str(), "Description": Str()}))})}),
 renameModule = (name: string, newName: string) => HTTPRequest(`/api/modules/${name}/${newName}`, {"method": "post"}),
 deleteModule = (name: string) => HTTPRequest("/api/modules/"+name, {"method": "delete"}),
-setType = (module: string, name: string, description: string, typ: Field[]) => HTTPRequest(`/api/modules/${module}/${name}`, {"method": "put", "data": JSON.stringify({description, "type": typ})});
+setType = (module: string, name: string, description: string, typ: Field[]) => HTTPRequest(`/api/modules/${module}/${name}`, {"method": "put", "data": JSON.stringify({description, "type": typ})}),
+getType = (module: string, name: string) => HTTPRequest(`/api/modules/${module}/${name}`, {"response": "json", "checker": Obj({"Name": Str(), "Description": Str(), "Fields": Arr(Field)})}),
+renameType = (module: string, name: string, newName: string) => HTTPRequest(`/api/modules/${module}/${name}/${newName}`, {"method": "post"}),
+deleteType = (module: string, name: string) => HTTPRequest(`/api/modules/${module}/${name}`, {"method": "delete"});
