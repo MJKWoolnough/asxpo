7a545047526b12a5d5ca24679e502a428aff5120
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-25 07:27:18 +0100

    initial types tests

---
+++ backend/types_test.go
@@ -0,0 +1,29 @@
+package backend
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestTypes(t *testing.T) {
+	tests(t, []test{
+		{ // 1
+			Method: http.MethodPut,
+			URL:    "/modules/some-module",
+			Body:   "some data",
+			Code:   http.StatusCreated,
+		},
+		{ // 2
+			Method:   http.MethodGet,
+			URL:      "/modules/some-module",
+			Code:     http.StatusOK,
+			Response: "{\"Name\":\"some-module\",\"Description\":\"some data\"}\n",
+		},
+		{ // 3
+			Method:   http.MethodPut,
+			URL:      "/modules/some-module/myType",
+			Code:     http.StatusBadRequest,
+			Response: "EOF\n",
+		},
+	})
+}
