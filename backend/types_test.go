7e401da4dd4fafae030b44a324cce6c1b0658a0e
Michael Woolnough <michael.woolnough@gmail.com>
2026-06-25 07:35:28 +0100

    added simple type create test

--- backend/types_test.go
+++ backend/types_test.go
@@ -25,5 +25,17 @@ func TestTypes(t *testing.T) {
 			Code:     http.StatusBadRequest,
 			Response: "EOF\n",
 		},
+		{ // 4
+			Method: http.MethodPut,
+			URL:    "/modules/some-module/myType",
+			Body:   `{"Description": "My Type", "Fields": [{"Name": "A", "Description": "A Field", "Type": "string"}, {"Name": "B", "Description": "B Field", "Type": "int"}]}`,
+			Code:   http.StatusNoContent,
+		},
+		{ // 5
+			Method:   http.MethodGet,
+			URL:      "/modules/some-module",
+			Code:     http.StatusOK,
+			Response: "{\"Name\":\"some-module\",\"Description\":\"some data\",\"Types\":[{\"Name\":\"myType\",\"Description\":\"My Type\"}]}\n",
+		},
 	})
 }
